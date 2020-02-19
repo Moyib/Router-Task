526df1fe21d54b88852ffa3391cbcc89a02db004
Moyib Ayomide <mideyyadetola@gmail.com>
2020-02-19 23:03:06 +0100

    few changes

--- src/App.vue
+++ src/App.vue
@@ -1,79 +1,56 @@
 <template>
   <div id="app">  
 
-      <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-      <b-icon-egg-fried font-scale="4"></b-icon-egg-fried>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
+     <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+        <b-icon-egg-fried font-scale="4"></b-icon-egg-fried>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
 
-      <div class="collapse navbar-collapse" id="navbarColor01">
-        <ul class="navbar-nav mr-auto">
-          <li class="nav-item active">
-            <router-link class="nav-link" to="/">Home </router-link>
-          </li>
-          <li class="nav-item">
-            <router-link class="nav-link" to="/about">About</router-link>
-          </li>
-          <li class="nav-item">
-            <router-link class="nav-link" to="/contact">Contact</router-link>
-          </li>
-          
-        </ul>
-        <form class="form-inline my-2 my-lg-0">
-          <input class="form-control mr-sm-2" type="text" placeholder="Search">
-          <button class="btn btn-secondary my-2 my-sm-0" type="submit">Search</button>
-        </form>
+        <div class="collapse navbar-collapse" id="navbarColor01">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+              <router-link class="nav-link" to="/">Home </router-link>
+            </li>
+            <li class="nav-item">
+              <router-link class="nav-link" to="/about">About</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link class="nav-link" to="/contact">Contact</router-link>
+            </li>
+            
+          </ul>
+          <form class="form-inline my-2 my-lg-0">
+            <input class="form-control mr-sm-2" type="text" placeholder="Search">
+            <button class="btn btn-secondary my-2 my-sm-0" type="submit">Search</button>
+          </form>
+        </div>
+      </nav>
+
+      <router-view></router-view>
+      <div class="tab">
+        <keep-alive>
+         <component v-bind:is="component"></component>
+        </keep-alive>
+       <button v-on:click="toggle">Let us serve you now</button>
+
+        <b-list-group>
+          <b-list-group-item href="#" v-for="item in items" :key="item">
+            {{ item.message }}
+          </b-list-group-item>
+          <b-list-group-item href="#">
+            Mochiato <b-badge>4 cups left</b-badge>
+          </b-list-group-item>
+        </b-list-group>
+
+        <div class="text-center">
+          <b-button variant="primary" size="sm" v-on:click="shoutout_count += 1">
+            <h5>Click here to give us a shoutout (or two 😉) <b-badge variant="bold"> {{ shoutout_count }}</b-badge></h5>
+          </b-button>
+        </div>
       </div>
-    </nav>
-
-    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-
-  <div class="collapse navbar-collapse" id="navbarColor01">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <router-link class="nav-link" to="/">Home </router-link>
-      </li>
-      <li class="nav-item">
-        <router-link class="nav-link" to="/about">About</router-link>
-      </li>
-      <li class="nav-item">
-        <router-link class="nav-link" to="/contact">Contact</router-link>
-      </li>
-      
-    </ul>
-    <form class="form-inline my-2 my-lg-0">
-      <input class="form-control mr-sm-2" type="text" placeholder="Search">
-      <button class="btn btn-secondary my-2 my-sm-0" type="submit">Search</button>
-    </form>
-  </div>
-</nav>
-
-    <router-view></router-view>
-   <div class="tab">
-      <keep-alive>
-        <component v-bind:is="component"></component>
-      </keep-alive>
-      <button v-on:click="toggle">Let us serve you now</button>
-
-      <b-list-group>
-        <b-list-group-item href="#" v-for="item in items" :key="item">
-          {{ item.message }}
-        </b-list-group-item>
-        <b-list-group-item href="#">
-          Mochiato <b-badge>4 cups left</b-badge>
-        </b-list-group-item>
-      </b-list-group>
-
-      <div class="text-center">
-        <b-button variant="primary" size="sm" v-on:click="shoutout_count += 1">
-          <h5>Click here to give us a shoutout (or two 😉) <b-badge variant="bold"> {{ shoutout_count }}</b-badge></h5>
-        </b-button>
-      </div>
-    </div>
+  
+    
   </div>
 </template>
 
